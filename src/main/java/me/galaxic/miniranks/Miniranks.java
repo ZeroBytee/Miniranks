071c879235e86e07c2e1e2a2ae4a952356d76a6e
Galaxic <96119080+ZeroBytee@users.noreply.github.com>
2022-08-10 08:17:54 +0200

    Optimized permsGui and adding/fixing some stuff

---
+++ src/main/java/me/galaxic/miniranks/Miniranks.java
@@ -0,0 +1,140 @@
+package me.galaxic.miniranks;
+
+import me.galaxic.miniranks.commands.playersCommand;
+import me.galaxic.miniranks.commands.rankCommand;
+import me.galaxic.miniranks.listeners.GuiListener;
+import me.galaxic.miniranks.listeners.chatListener;
+import me.galaxic.miniranks.listeners.onJoin;
+import me.galaxic.miniranks.managers.NametagManager;
+import me.galaxic.miniranks.managers.RankManager;
+import me.galaxic.miniranks.tabCompleter.rankTab;
+import me.galaxic.miniranks.utils.rankGuiUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
+public final class Miniranks extends JavaPlugin {
+
+    private Database database;
+    private RankManager rankManager;
+    private NametagManager nametagManager;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+
+        database = new Database(this);
+        rankManager = new RankManager(this);
+        nametagManager = new NametagManager(this);
+
+        try {
+            database.connect();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        if (database.isConnected()) {
+            System.out.println("[MiniRanks Debug] Connected to database");
+            database.createTable();
+        }
+        loadCommands();
+        Metrics metrics = new Metrics(this, 16048);
+
+        rankManager.addTestPerms("&4Owner");
+        ArrayList<String> ranks = rankManager.getPerms("&4Owner");
+        ranks.add("miniranks.lol.owner");
+        rankManager.addPerms(ranks, "&4Owner");
+
+
+
+
+        // loop to make sure the database doesn't disconnect
+        int task = Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                // select * from table where id = 1
+                try {
+                    PreparedStatement ps = getDatabase().getConnection().prepareStatement("SELECT * FROM " + getTable() + " WHERE ID = 1");
+                    ResultSet rs = ps.executeQuery();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, 0L, 240L);
+    }
+
+    public Database getDatabase() {
+        return database;
+    }
+    public RankManager getRankManager() {
+        return rankManager;
+    }
+    public NametagManager getNametagManager() {
+        return nametagManager;
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        database.disconnect();
+    }
+
+    public void loadCommands() {
+        // register join event
+        getServer().getPluginManager().registerEvents(new onJoin(this), this);
+        getServer().getPluginManager().registerEvents(new chatListener(this), this);
+        getServer().getPluginManager().registerEvents(new GuiListener(this), this);
+        // register commands
+        Objects.requireNonNull(getCommand("miniranks")).setExecutor(new rankCommand(this));
+        // tab complete for rank command
+        Objects.requireNonNull(getCommand("miniranks")).setTabCompleter(new rankTab(this));
+
+
+        Objects.requireNonNull(getCommand("players")).setExecutor(new playersCommand(this));
+    }
+
+    // Database getters
+    private final String HOST = getConfig().getString("database-settings.host");
+    private final String USER = getConfig().getString("database-settings.username");
+    private final String PASSWORD = getConfig().getString("database-settings.password");
+    private final String DATABASE = getConfig().getString("database-settings.database");
+    private final int PORT = getConfig().getInt("database-settings.port");
+    private final String TABLE = getConfig().getString("database-settings.table");
+    private final String RANK_TABLE = getConfig().getString("database-settings.rank-table");
+    private final String defaultRank = getConfig().getString("rank-settings.default-rank");
+
+    public String getDATABASE() {
+        return DATABASE;
+    }
+    public String getHost() {
+        return HOST;
+    }
+    public String getUser() {
+        return USER;
+    }
+    public String getPassword() {
+        return PASSWORD;
+    }
+    public int getPort() {
+        return PORT;
+    }
+    public String getTable() {
+        return TABLE;
+    }
+    public String getRankTable() {
+        return RANK_TABLE;
+    }
+    public String getDefaultRank() {
+        return defaultRank;
+    }
+}
+
+
+
---
+++ src/main/java/me/galaxic/miniranks/tabCompleter/rankTab.java
@@ -0,0 +1,57 @@
+package me.galaxic.miniranks.tabCompleter;
+
+import me.galaxic.miniranks.Miniranks;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class rankTab implements TabCompleter {
+
+    private Miniranks miniranks;
+    public rankTab(Miniranks plugin) {
+        this.miniranks = plugin;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+
+        if (args.length == 1) {
+            return StringUtil.copyPartialMatches(args[0], Arrays.asList("set", "create", "delete", "perms", "help"), new ArrayList<>());
+        } else if (args.length == 2) {
+
+            if (args[0].equalsIgnoreCase("set")) {
+                List<String> names = new ArrayList<>();
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    names.add(player.getName());
+                }
+                return StringUtil.copyPartialMatches(args[1], names, new ArrayList<>());
+            } else if (args[0].equalsIgnoreCase("create")) {
+                return StringUtil.copyPartialMatches(args[1], Collections.singletonList("<rank>"), new ArrayList<>());
+            } else if (args[0].equalsIgnoreCase("delete")) {
+                List<String> ranks = miniranks.getRankManager().getRanks();
+                return StringUtil.copyPartialMatches(args[1], ranks, new ArrayList<>());
+            } else if (args[0].equalsIgnoreCase("perms")) {
+                List<String> ranks = miniranks.getRankManager().getRanks();
+                return StringUtil.copyPartialMatches(args[1], ranks, new ArrayList<>());
+            }
+        } else if (args.length == 3) {
+            if (args[0].equalsIgnoreCase("set")) {
+                // get all the ranks from the database and for each rank, add it to the list
+                List<String> ranks = miniranks.getRankManager().getRanks();
+                return StringUtil.copyPartialMatches(args[2], ranks, new ArrayList<>());
+            } else if (args[0].equalsIgnoreCase("create")) {
+                return StringUtil.copyPartialMatches(args[2], Collections.singletonList("<prefix>"), new ArrayList<>());
+            }
+        }
+
+        return new ArrayList<>();
+    }
+}
